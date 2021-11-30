f410e98ef3161cea9fbaf2f129fb9741f34a34ef
SydNS <sydneynsiimbe@gmail.com>
2021-11-30 16:30:37 +0300

    working with heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import DATABASES as DATABASES
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import DATABASES as DATABASES
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
